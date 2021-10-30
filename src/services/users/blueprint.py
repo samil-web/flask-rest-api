23e3132c326aef15b32da391aa0d12d1eea2fab7
Shamil <samil.alieff@gmail.com>
2021-10-30 21:42:21 +0400

    rest_api

--- src/services/users/blueprint.py
+++ src/services/users/blueprint.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 
 from flask_restful import Api
 
-from controller import UsersHandler,UserHandler
+from src.services.users.controller import UsersHandler,UserHandler
 
 """
 
